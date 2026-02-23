b60acbbfffe07214c3725c6f54efa99c6f1c59cd
Omygod20 <2352620@csfoy.ca>
2026-02-23 13:41:29 -0500

    migrations for all tables done

---
+++ tp1_guerette_etienne/database/migrations/2026_02_23_182315_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('rating');
+            $table->text('comment')->nullable();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('rental_id')->references('id')->on('rentals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
