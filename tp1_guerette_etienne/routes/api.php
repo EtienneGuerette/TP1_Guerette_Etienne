0022cba8f4e2af45ee154868fad6113f64380754
Omygod20 <2352620@csfoy.ca>
2026-03-02 12:54:27 -0500

    api routes done

---
+++ tp1_guerette_etienne/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\RentalController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/equipment', [EquipmentController::class, 'index']);
+Route::get('/equipment/{id}', [EquipmentController::class, 'show']);
+
+Route::get('/equipment/{id}/popularity', [EquipmentController::class, 'popularity']);
+
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+
+Route::delete('/reviews/{id}', [ReviewController::class, 'destroy']);
+
+Route::get('/rentals/{id}/average_price', [RentalController::class, 'averagePrice']);
