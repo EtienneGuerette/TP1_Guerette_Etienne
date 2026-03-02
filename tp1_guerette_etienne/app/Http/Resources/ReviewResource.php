53f89fa546d1d3eb89335993e4fada3d54e0c222
Omygod20 <2352620@csfoy.ca>
2026-03-02 12:28:44 -0500

    resources done

---
+++ tp1_guerette_etienne/app/Http/Resources/ReviewResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'comment' => $this->comment,
+            'rating' => $this->rating,
+            'user_id' => $this->user_id,
+            'rental_id' => $this->rental_id,
+        ];
+    }
+}
